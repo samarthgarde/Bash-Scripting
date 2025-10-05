e00731d80c9801762691bd9b03f0571c8b108922
samarthgarde007 <samarthgarde007@gmail.com>
2025-10-05 16:56:35 +0530

    2nd day learning

---
+++ string_oprations/string-ops.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+myVar="hello world!"
+
+myVar2="SAMARTH"
+
+length=${#myVar}
+
+upper=${myVar^^}
+lower=${myVar2,,}
+
+echo "$upper"
+echo "$lower"
+echo "$length"
+replace=${myVar/world/Guys}
+
+echo $replace
+
+slice=${myVar:0:11}
+
+echo $slice

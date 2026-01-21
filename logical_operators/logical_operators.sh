0a15345976c51ad81d97b023b9f59fd8ae929c05
samarthgarde007 <samarthgarde007@gmail.com>
2026-01-21 10:17:26 +0530

    update logical_operator

--- logical_operators/logical_operators.sh
+++ logical_operators/logical_operators.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#
-echo "Enter a number : "
+
+echo "Enter a number:"
 read num
 
-if [ $num -gt 0 ] $$ [ $num -lt 100 ]; then
-	echo "Number is Between 1 to 99"
-fi 
-if [ $num -lt 0 ] || [ $num -gt 100 ]; then
-        echo "Number is outside 1-100"
+if [ "$num" -gt 0 ] && [ "$num" -lt 100 ]; then
+    echo "Number is between 1 and 99"
 fi
-if [ $num -eq 50 ]; then
-  	echo "Number is not 50"
+
+if [ "$num" -lt 0 ] || [ "$num" -gt 100 ]; then
+    echo "Number is outside 1 to 100"
 fi
+
+if [ "$num" -eq 50 ]; then
+    echo "Number is 50"
+fi
+

60229c0779b0ba2307f2a779b5d6c7ecc6af79ec
samarthgarde007 <samarthgarde007@gmail.com>
2026-01-21 10:20:46 +0530

    string_operations

--- string_operations/string_operations.sh
+++ string_operations/string_operations.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 myVar="hello world!"
-
 myVar2="SAMARTH"
 
 length=${#myVar}
@@ -12,10 +11,10 @@ lower=${myVar2,,}
 echo "$upper"
 echo "$lower"
 echo "$length"
-replace=${myVar/world/Guys}
 
-echo $replace
+replace=${myVar/world/Guys}
+echo "$replace"
 
 slice=${myVar:0:11}
+echo "$slice"
 
-echo $slice

24f47f6aeae50ab9864387f87f3e3ff85dd7749a
samarthgarde007 <samarthgarde007@gmail.com>
2026-01-21 10:37:51 +0530

    enhance the files

--- Functions/even_odd_number.sh
+++ Functions/even_odd_number.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 is_even() {
-	if (( $1 % 2 == 0 )); then
-		return 0
-	else
-		return 1
-	fi
+    if (( $1 % 2 == 0 )); then
+        return 0
+    else
+        return 1
+    fi
 }
-echo "Enter an Number: "
-read n
 
-if is_even $n; then
-	echo "$n is even "
-else
-	echo "$n is odd"
+read -p "Enter a number: " n
+
+# Validate input
+if ! [[ "$n" =~ ^-?[0-9]+$ ]]; then
+    echo "Please enter a valid integer"
+    exit 1
 fi
+
+if is_even "$n"; then
+    echo "$n is even"
+else
+    echo "$n is odd"
+fi
+
